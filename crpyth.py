4ccdbd5d526294122959567e60f13a69905f47ea
Christophe Narbonne <christophe31@gmail.com>
2009-05-16 00:12:56 +0200

    add a ceaser/ascii encription like to replace

--- crpyth.py
+++ crpyth.py
@@ -1,5 +1,9 @@
 #/usr/bin/env python
 import math
-import system
 
 message=raw_input('message?')
+k=int(raw_input('clef'))
+mk=""
+for i in [chr(ord(i)+k%256) for i in message]:
+    mk+=i
+print mk
